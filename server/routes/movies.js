93f32a9532825151716a491840ae2353a1c4f269
David Brown <david.brown@windowslive.com>
2019-07-18 15:06:42 +1200

    Completed external API call function

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -5,16 +5,22 @@ const db = require('../db/db')
 
 const router = express.Router()
 const apiUrl = 'https://api.themoviedb.org/3/movie/'
-const apiKey = process.env.API_KEY
+const apiKey = require('./apiKey')
 
 const getMoviesFromAPI = (id) => {
-  return request.get(`${apiUrl}${id}?api_key=${apiKey}&language=en-US`).then(movie => {
+  return request.get(`${apiUrl}${id}?api_key=${apiKey}&language=en-US`).then(({ body }) => {
     return {
-      
+      releaseDate: body.release_date,
+      description: body.overview,
+      rating: body.vote_average,
+      genres: body.genres
     }
-    }
+  }).then(movie => {
+    return request.get(`${apiUrl}${id}/credits?api_key=${apiKey}`).then(({ body }) => {
+      movie.cast = body.cast
+      return movie
+    })
   })
 }
-
 
 module.exports = router
