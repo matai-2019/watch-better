925e3074df37f7f2a3c0119f1be4fb0a87a0ffd6
Jennie-Watson <watson.jennie17@gmail.com>
2019-07-18 14:11:49 +1200

    getMovie by id function written

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,25 @@
 const config = require('./knexfile').development
 const database = require('knex')(config)
 
+// const connection = require('./connection')
+
 const close = (db = database) => {
   db.destroy()
 }
 
+function getMovie (id, db = database) {
+  return db('movies')
+    .where('movies.id', id)
+    .first()
+    .select(
+      'movies.id',
+      'movies.title',
+      'movies.API_movie_id',
+      'movies.recommended'
+    )
+}
+
 module.exports = {
-  close
+  close,
+  getMovie
 }
