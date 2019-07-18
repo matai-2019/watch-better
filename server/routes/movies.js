0d8e0e92c4df2697064852181031aaf17903c741
Jared <jared@needapart.co.nz>
2019-07-18 13:49:36 +1200

    Refactor spaces

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -4,12 +4,12 @@ const db = require('../db/db')
 
 const router = express.Router()
 
-router.get('/:id',(req,res) => {
+router.get('/:id', (req, res) => {
   const { id } = req.params
-    res.send(`returns movie with id : ${id}`)
-  })
+  res.send(`returns movie with id : ${id}`)
+})
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
   res.send('return a list of movies')
 })
 
