497108b322efb17ea5e5663c63d5d81a5a280e45
David Brown <david.brown@windowslive.com>
2019-07-18 16:29:58 +1200

    All fixed

--- src/tests/movies.test.js
+++
@@ -1,8 +0,0 @@
-const getMoviesFromAPI = require('../../server/routes/movies')
-
-test('getSeasons returns all seasons', () => {
-  return getMoviesFromAPI(550)
-    .then((data) => {
-      expect(data.rating).toEqual(8.4)
-    })
-})
--- tests/server/routes/movies.test.js
+++
@@ -1,8 +0,0 @@
-const blah = require('../../../server/routes/movies')
-
-test('getSeasons returns all seasons', () => {
-  return blah.getMoviesFromAPI(550)
-    .then(({ body }) => {
-      expect(body.id).toEqual(550)
-    })
-})
