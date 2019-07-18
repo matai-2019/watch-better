053c53b7ec226758035ca88f2c482bff852c2e22
David Brown <david.brown@windowslive.com>
2019-07-18 12:44:33 +1200

    mid change

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const request = require('superagent')
 
 const db = require('../db/db')
 
 const router = express.Router()
+const apiUrl = 'https://api.themoviedb.org/3/movie/'
+const apiKey = process.env.API_KEY
 
+const getMoviesFromAPI = (id) => {
+  return request.get(`${apiUrl}${id}?api_key=${apiKey}&language=en-US`).then(movie => {
+    return {
+      
+    }
+    }
+  })
+}
 
 
 module.exports = router
