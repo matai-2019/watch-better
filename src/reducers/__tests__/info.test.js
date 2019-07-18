b26b3c9677b121b9e937f1166983c5400620b7ee
Joanna Klimas <klimas.j.m@gmail.com>
2019-07-18 17:22:10 +1200

    fix info reducer test

--- src/reducers/__tests__/info.test.js
+++ src/reducers/__tests__/info.test.js
@@ -14,46 +14,52 @@ import infoReducer from '../info'
 
 describe('infoReducer with getMovieDetails actions', () => {
   it('returns pending | pending: true', () => {
-    const intialState = { pending: true }
+    const testState = { pending: true }
+    const initialState = { pending: false }
     const action = { type: GET_MOVIE_DETAILS_PENDING, action: {} }
-    const actual = infoReducer(intialState, action)
-    expect(actual).toStrictEqual(intialState)
+    const actual = infoReducer(initialState, action)
+    expect(actual).toStrictEqual(testState)
   })
 
-  it('returns success | pending: false, error: null', () => {
-    const intialState = { pending: false, error: null }
+  it('returns success | pending: false', () => {
+    const testState = { pending: false }
+    const intialState = { pending: true }
     const action = { type: GET_MOVIE_DETAILS_SUCCESS, action: {} }
     const actual = infoReducer(intialState, action)
-    expect(actual).toStrictEqual(intialState)
+    expect(actual).toStrictEqual(testState)
   })
 
   it('returns error | pending: false, error: message', () => {
-    const intialState = { pending: false, error: 'ERROR' }
+    const testState = { pending: false, error: 'ERROR' }
+    const intialState = { pending: true, error: null }
     const action = { type: GET_MOVIE_DETAILS_ERROR, message: 'ERROR' }
     const actual = infoReducer(intialState, action)
-    expect(actual).toStrictEqual(intialState)
+    expect(actual).toStrictEqual(testState)
   })
 })
 
 describe('infoReducer with getMovies actions', () => {
   it('returns pending | pending: true', () => {
-    const intialState = { pending: true }
+    const testState = { pending: true }
+    const intialState = { pending: false }
     const action = { type: GET_MOVIES_PENDING, action: {} }
     const actual = infoReducer(intialState, action)
-    expect(actual).toStrictEqual(intialState)
+    expect(actual).toStrictEqual(testState)
   })
 
-  it('returns success | pending: false, error: null', () => {
-    const intialState = { pending: false, error: null }
+  it('returns success | pending: false', () => {
+    const testState = { pending: false }
+    const intialState = { pending: true }
     const action = { type: GET_MOVIES_SUCCESS, action: {} }
     const actual = infoReducer(intialState, action)
-    expect(actual).toStrictEqual(intialState)
+    expect(actual).toStrictEqual(testState)
   })
 
   it('returns error | pending: false, error: message', () => {
-    const intialState = { pending: false, error: 'ERROR' }
+    const testState = { pending: false, error: 'ERROR' }
+    const intialState = { pending: true, error: null }
     const action = { type: GET_MOVIES_ERROR, message: 'ERROR' }
     const actual = infoReducer(intialState, action)
-    expect(actual).toStrictEqual(intialState)
+    expect(actual).toStrictEqual(testState)
   })
 })
