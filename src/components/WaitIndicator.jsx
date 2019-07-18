834b2591736c1f426814a75848d10af9045dad92
Jennie-Watson <watson.jennie17@gmail.com>
2019-07-18 17:25:56 +1200

    WaitIndicator component in progress

---
+++ src/components/WaitIndicator.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+const WaitIndicator = makeStyles(theme => ({
+  progress: {
+    margin: theme.spacing(2),
+  },
+}))
+
+export default function CircularIndeterminate() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <CircularProgress className={classes.progress} />
+      <CircularProgress className={classes.progress} color="secondary" />
+    </div>
+  )
+}
+
+export default WaitIndicator
+
