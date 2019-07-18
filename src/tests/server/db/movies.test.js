9be66f30f09c16d0dc90981a9c4267eb22dd3245
Jennie-Watson <watson.jennie17@gmail.com>
2019-07-19 10:12:53 +1200

    test written for all movie data with tables joined

--- src/tests/server/db/movies.test.js
+++ src/tests/server/db/movies.test.js
@@ -15,9 +15,22 @@ test('db.getMovie returns a specific movie', () => {
     const expected = {
       id: 1,
       title: 'A Star is Born',
-      API_movie_id: 'null',
+      API_movie_id: 1,
       recommended: 'true'
     }
     expect(movie).toEqual(expected)
   })
-})
+}) // id changed from 1 to check if test fails and test still passing
+
+test('db.getMovie returns all data for a specific movie', () => {
+  db.getMovie(3, testDb).then(movie => {
+    const expected = {
+      movie_id: 3,
+      title: 'Annihilation',
+      recommended: 'true',
+      testType: 'Bechdel Test',
+      result: true
+    }
+    expect(movie).toEqual(expected)
+  })
+}) // id changed from 3 to check if test fails and test still passing
