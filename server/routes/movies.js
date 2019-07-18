84a534977e298e77922d5993ac0914b09b781994
Jared <jared@needapart.co.nz>
2019-07-18 12:58:32 +1200

    Set up API routes for / and /:id

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -4,6 +4,13 @@ const db = require('../db/db')
 
 const router = express.Router()
 
+router.get('/:id',(req,res) => {
+  const { id } = req.params
+    res.send(`returns movie with id : ${id}`)
+  })
 
+router.get('/', (req,res) => {
+  res.send('return a list of movies')
+})
 
 module.exports = router
