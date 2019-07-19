a331c0070467dfdb43a786760059c1b5718e928c
Jared <jared@needapart.co.nz>
2019-07-19 15:03:54 +1200

    Make a '/search/:id/ route

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -1,9 +1,7 @@
 const express = require('express')
-const request = require('superagent')
-
 const router = express.Router()
-const apiUrl = 'https://api.themoviedb.org/3/movie/'
-const apiKey = require('./apiKey')
+
+const { findMovieFromAPI, getMoviesFromAPI } = require('./routeHelpers')
 
 router.get('/:id', (req, res) => {
   const { id } = req.params
@@ -15,24 +13,18 @@ router.get('/:id', (req, res) => {
     })
 })
 
+router.get('/search/:query', (req, res) => {
+  const { query } = req.params
+  findMovieFromAPI(query)
+    .then(movie => res.send(movie))
+    .catch(err => {
+      res.status(500).send('Consuming TMDB failed')
+      console.error(err)
+    })
+})
+
 router.get('/', (req, res) => {
   res.send('return a list of movies')
 })
-
-const getMoviesFromAPI = (id) => {
-  return request.get(`${apiUrl}${id}?api_key=${apiKey}&language=en-US`).then(({ body }) => {
-    return {
-      releaseDate: body.release_date,
-      description: body.overview,
-      rating: body.vote_average,
-      genres: body.genres
-    }
-  }).then(movie => {
-    return request.get(`${apiUrl}${id}/credits?api_key=${apiKey}`).then(({ body }) => {
-      movie.cast = body.cast
-      return movie
-    })
-  })
-}
 
 module.exports = router
