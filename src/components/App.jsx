5a1103e93f3759c4c31cb18fc76a2171121398c3
Raaya Pathare <raaya.pathare7@gmail.com>
2019-07-19 15:30:00 +1200

    fix pr requests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,8 @@ import { CssBaseline } from '@material-ui/core'
 import { ThemeProvider } from '@material-ui/styles'
 import { connect } from 'react-redux'
 import { theme } from '../style/muiStyles'
-import MovieList from './MovieList'
 
+import MovieList from './MovieList'
 import NotFound from './NotFound'
 import Header from './Header'
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { getMoviesSuccess } from '../actions/movies'
 import PropTypes from 'prop-types'
+
 import MovieListItem from './MovieListItem'
 
 class MovieList extends React.Component {
