9fd0b93fed2b8dd551c34deed7bcd98839cc04f5
Joanna Klimas <klimas.j.m@gmail.com>
2019-07-21 11:24:16 +1200

    tests for Quiz component

---
+++ src/tests/client/components/Quiz.test.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+import Enzyme, { mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+import Quiz from '../../../components/Quiz'
+import QuizQuestion from '../../../components/QuizQuestion'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('Quiz component tests', () => {
+  it('<Quiz /> displays a button on a initial render', () => {
+    const wrapper = mount(
+      <Quiz />
+    )
+    const actual = wrapper.find(Button)
+    expect(actual).toHaveLength(1)
+  })
+  it('<Quiz> component changes this.state.isVisible to false on start button click', () => {
+    const expected = false
+    const wrapper = mount(<Quiz/>)
+    const startButton = wrapper.find('button').at(0)
+
+    startButton.simulate('click')
+
+    expect(wrapper.state().isVisible).toEqual(expected)
+  })
+  it('<Quiz> component displays 17 <QuizQuestion> components when this.state.isVisible is set to false', () => {
+    const expected = 17
+    const wrapper = mount(<Quiz/>)
+    wrapper.setState({ isVisible: false })
+
+    const actual = wrapper.find(QuizQuestion)
+    expect(actual).toHaveLength(expected)
+  })
+})
