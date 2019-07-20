e5add3fac7f1f1df47a68cb6c71cc5f9255b26f7
Joanna Klimas <klimas.j.m@gmail.com>
2019-07-20 13:49:05 +1200

    create data file

---
+++ src/data/quizQuestions.js
@@ -0,0 +1,87 @@
+export default [
+  {
+    id: '1',
+    question: 'Does [name of movie] have two women, that at some point have a conversation with each other?',
+    test: 'Bechdel'
+  },
+  {
+    id: '2',
+    question: 'Is this conversation about something other than a man?',
+    test: 'Bechdel'
+  },
+  {
+    id: '3',
+    question: 'Does [name of movie] have at least two women in each department of movie production?',
+    test: 'Rees Davies'
+  },
+  {
+    id: '4',
+    question: 'Is there a female identifying person in the film who is non-white?',
+    test: 'Ko'
+  },
+  {
+    id: '5',
+    question: 'Does that person speak in at least five scenes?',
+    test: 'Ko'
+  },
+  {
+    id: '6',
+    question: 'Do they speak English?',
+    test: 'Ko'
+  },
+  {
+    id: '7',
+    question: 'Is there a primary female character?',
+    test: 'Landau'
+  },
+  {
+    id: '8',
+    question: 'Do they end up dead or pregnant?',
+    test: 'Landau'
+  },
+  {
+    id: '9',
+    question: 'Do they ever cause a plot problem for a male protagonist?',
+    test: 'Landau'
+  },
+  {
+    id: '10',
+    question: 'Is the director of [name of movie] female?',
+    test: 'Feldman'
+  },
+  {
+    id: '11',
+    question: 'Is there a female writer?',
+    test: 'Feldman'
+  },
+  {
+    id: '12',
+    question: 'Is there a female composer or director of photography?',
+    test: 'Feldman'
+  },
+  {
+    id: '13',
+    question: 'Are there at least three female producers or three female department heads?',
+    test: 'Feldman'
+  },
+  {
+    id: '14',
+    question: 'Is the crew at least 50% women?',
+    test: 'Feldman'
+  },
+  {
+    id: '15',
+    question: 'Is there a female protagonist that determines story outcomes?',
+    test: 'Feldman'
+  },
+  {
+    id: '16',
+    question: 'Are any female characters victimized, stereotyped or sexualized?',
+    test: 'Feldman'
+  },
+  {
+    id: '17',
+    question: 'Is there a female character that initiates or reciprocates sexual',
+    test: 'Feldman'
+  }
+]
