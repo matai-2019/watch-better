8e26417148296182e03a35a851607cf7c2edb408
Anisha Kalidas <anishadhana@gmail.com>
2019-07-20 18:21:58 +1200

    corrected spelling

--- src/components/Sorter.jsx
+++ src/components/Sorter.jsx
@@ -3,7 +3,7 @@ import { Select, FormControl, InputLabel, MenuItem } from '@material-ui/core'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-import { SORT_RAITING_HIGH_LOW, SORT_RAITING_LOW_HIGH, SORT_ALPHABETICAL_ASCENDING, sortBy } from '../actions/sort'
+import { SORT_RATING_HIGH_LOW, SORT_RATING_LOW_HIGH, SORT_ALPHABETICAL_ASCENDING, sortBy } from '../actions/sort'
 import { SorterStyle } from '../style/muiStyles'
 
 const Sorter = ({ dispatch }) => {
@@ -24,8 +24,8 @@ const Sorter = ({ dispatch }) => {
         }}
       >
         <MenuItem value={SORT_ALPHABETICAL_ASCENDING}>Sort A-Z</MenuItem>
-        <MenuItem value={SORT_RAITING_HIGH_LOW}>Raiting High to Low</MenuItem>
-        <MenuItem value={SORT_RAITING_LOW_HIGH}>Raiting Low to High</MenuItem>
+        <MenuItem value={SORT_RATING_HIGH_LOW}>Rating High to Low</MenuItem>
+        <MenuItem value={SORT_RATING_LOW_HIGH}>Rating Low to High</MenuItem>
       </Select>
     </FormControl>
   )
