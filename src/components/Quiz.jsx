92bb49cf0a63b8d5cc15fcca981f031640c3b356
Joanna Klimas <klimas.j.m@gmail.com>
2019-07-20 13:48:42 +1200

    refactor a quiz component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,17 +1,75 @@
 import React from 'react'
 import { Radio, RadioGroup, FormControlLabel, FormControl, FormLabel, TextField, Button } from '@material-ui/core'
 
+import QuizQuestion from './QuizQuestion'
+import data from '../data/quizQuestions.js'
+
 class Quiz extends React.Component {
 
   state = {
     title: '',
-    isVisible: true
+    isVisible: true,
+    answers: {
+      t1: [],
+      t2: []
+    }
   }
 
   handleName = e => {
     this.setState({
       title: e.target.value
     })
+  }
+  
+  test1 = []
+  test2 = []
+  test3 = []
+  test4 = []
+  test5 = []
+ 
+
+  handleChange = (value, id) => {
+  console.log(`test1: ${this.test1}`)
+  console.log(`test2: ${this.test2}`)
+  console.log(`test3: ${this.test3}`)
+  console.log(`test4: ${this.test4}`)
+  console.log(`test5: ${this.test5}`)
+
+    switch (id) {
+      case '1':
+      case '2':
+        this.test1.push(value)
+        return this.test1
+      break
+        this.test1.push(value)
+        return this.test1
+      break
+      case '3':
+        this.test2.push(value)
+        return this.test2 
+      case '4':
+      case '5': 
+      case '6':
+        this.test3.push(value)
+        return this.test3 
+      case '7':
+      case '8': 
+      case '9':
+        this.test4.push(value)
+        return this.test4
+      case '10':
+      case '11': 
+      case '12':
+      case '13':
+      case '14': 
+      case '15':
+      case '16': 
+      case '17':
+        this.test5.push(value)
+        return this.test5
+      default:
+      console.log('whoops')
+    }
   }
 
   handleSubmit = () => {
@@ -22,7 +80,7 @@ class Quiz extends React.Component {
 
   render () {
     return this.state.isVisible ? 
-    <>
+      <>
         <TextField
           id="standard-search"
           label="Movie Title"
@@ -34,248 +92,10 @@ class Quiz extends React.Component {
         <Button onClick={this.handleSubmit}variant="outlined" color="primary">Start</Button>
         <br />
         <br />
-        </>
-        : <><FormControl component="fieldset">
-          <FormLabel component="legend">Does {this.state.title} have two women, that at some point have a conversation with each other?</FormLabel>
-          <RadioGroup
-            aria-label="Bechdel"
-          // className={classes.group}
-          // value={value}
-          // onChange={handleChange}
-          >
-            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-            <FormControlLabel value="No" control={<Radio />} label="No" />
-          </RadioGroup>
-        </FormControl>
-        <br/>
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Is this conversation about something other than a man?</FormLabel>
-          <RadioGroup
-            aria-label="Bechdel"
-          // className={classes.group}
-          // value={value}
-          // onChange={handleChange}
-          >
-            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-            <FormControlLabel value="No" control={<Radio />} label="No" />
-          </RadioGroup>
-        </FormControl>
-        <br/>
-
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Does {this.state.title} have at least two women in each department of movie production?</FormLabel>
-          <RadioGroup
-            aria-label="Rees Davies"
-          // className={classes.group}
-          // value={value}
-          // onChange={handleChange}
-          >
-            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-            <FormControlLabel value="No" control={<Radio />} label="No" />
-            <FormControlLabel value="I don't know" control={<Radio />} label="I don't know" />
-          </RadioGroup>
-        </FormControl>
-        <br/>
-
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Is there a female identifying person in the film who is non-white?</FormLabel>
-          <RadioGroup
-            aria-label="Ko"
-          // className={classes.group}
-          // value={value}
-          // onChange={handleChange}
-          >
-            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-            <FormControlLabel value="No" control={<Radio />} label="No" />
-          </RadioGroup>
-        </FormControl>
-        <br/>
-
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Does that person speak in at least five scenes?</FormLabel>
-          <RadioGroup
-            aria-label="Ko"
-          // className={classes.group}
-          // value={value}
-          // onChange={handleChange}
-          >
-            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-            <FormControlLabel value="No" control={<Radio />} label="No" />
-          </RadioGroup>
-        </FormControl>
-        <br/>
-
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Do they speak English?</FormLabel>
-          <RadioGroup
-            aria-label="Ko"
-          // className={classes.group}
-          // value={value}
-          // onChange={handleChange}
-          >
-            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-            <FormControlLabel value="No" control={<Radio />} label="No" />
-          </RadioGroup>
-        </FormControl>
-        <br/>
-
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Is there a primary female character?</FormLabel>
-          <RadioGroup
-            aria-label="Landau"
-          // className={classes.group}
-          // value={value}
-          // onChange={handleChange}
-          >
-            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-            <FormControlLabel value="No" control={<Radio />} label="No" />
-          </RadioGroup>
-        </FormControl>
-        <br/>
-
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Do they end up dead or pregnant?</FormLabel>
-          <RadioGroup
-            aria-label="Landau"
-          // className={classes.group}
-          // value={value}
-          // onChange={handleChange}
-          >
-            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-            <FormControlLabel value="No" control={<Radio />} label="No" />
-          </RadioGroup>
-        </FormControl>
-        <br/>
-
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Do they ever cause a plot problem for a male protagonist? </FormLabel>
-          <RadioGroup
-            aria-label="Landau"
-          // className={classes.group}
-          // value={value}
-          // onChange={handleChange}
-          >
-            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-            <FormControlLabel value="No" control={<Radio />} label="No" />
-          </RadioGroup>
-        </FormControl>
-        <br/>
-
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Is the director of {this.state.title} female? </FormLabel>
-          <RadioGroup
-            aria-label="Feldman"
-          // className={classes.group}
-          // value={value}
-          // onChange={handleChange}
-          >
-            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-            <FormControlLabel value="No" control={<Radio />} label="No" />
-          </RadioGroup>
-        </FormControl>
-        <br/>
-
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Is there a female writer? </FormLabel>
-          <RadioGroup
-            aria-label="Feldman"
-          // className={classes.group}
-          // value={value}
-          // onChange={handleChange}
-          >
-            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-            <FormControlLabel value="No" control={<Radio />} label="No" />
-          </RadioGroup>
-        </FormControl>
-        <br/>
-
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Is there a female composer or director of photography? </FormLabel>
-          <RadioGroup
-            aria-label="Feldman"
-          // className={classes.group}
-          // value={value}
-          // onChange={handleChange}
-          >
-            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-            <FormControlLabel value="No" control={<Radio />} label="No" />
-          </RadioGroup>
-        </FormControl>
-        <br/>
-
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Are there at least three female producers or three female department heads?</FormLabel>
-          <RadioGroup
-            aria-label="Feldman"
-          // className={classes.group}
-          // value={value}
-          // onChange={handleChange}
-          >
-            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-            <FormControlLabel value="No" control={<Radio />} label="No" />
-            <FormControlLabel value="I don't know" control={<Radio />} label="I don't know" />
-          </RadioGroup>
-        </FormControl>
-        <br/>
-
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Is the crew at least 50% women?</FormLabel>
-          <RadioGroup
-            aria-label="Feldman"
-          // className={classes.group}
-          // value={value}
-          // onChange={handleChange}
-          >
-            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-            <FormControlLabel value="No" control={<Radio />} label="No" />
-            <FormControlLabel value="I don't know" control={<Radio />} label="I don't know" />
-
-          </RadioGroup>
-        </FormControl>
-        <br/>
-
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Is there a female protagonist that determines story outcomes?</FormLabel>
-          <RadioGroup
-            aria-label="Feldman"
-          // className={classes.group}
-          // value={value}
-          // onChange={handleChange}
-          >
-            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-            <FormControlLabel value="No" control={<Radio />} label="No" />
-          </RadioGroup>
-        </FormControl>
-        <br/>
-
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Are any female characters victimized, stereotyped or sexualized?</FormLabel>
-          <RadioGroup
-            aria-label="Feldman"
-          // className={classes.group}
-          // value={value}
-          // onChange={handleChange}
-          >
-            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-            <FormControlLabel value="No" control={<Radio />} label="No" />
-          </RadioGroup>
-        </FormControl>
-        <br/>
-
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Is there a female character that initiates or reciprocates sexual?</FormLabel>
-          <RadioGroup
-            aria-label="Feldman"
-          // className={classes.group}
-          // value={value}
-          // onChange={handleChange}
-          >
-            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
-            <FormControlLabel value="No" control={<Radio />} label="No" />
-          </RadioGroup>
-        </FormControl></>
+      </>
+        : data.map(el => <QuizQuestion key={el.id} handleChange={this.handleChange} question={el.question} id={el.id} test={el.test} />)
   }
-  }
+}
 
 
 export default Quiz
