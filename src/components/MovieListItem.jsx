57d6ddf59b24f149132efe58ced5ed408fb85b8d
Anisha Kalidas <anishadhana@gmail.com>
2019-07-21 19:20:17 +1200

    beautiful buttons

--- src/components/MovieListItem.jsx
+++ src/components/MovieListItem.jsx
@@ -25,9 +25,27 @@ const MovieListItem = (props) => {
   const classes = MovieListItemStyles(props)
 
   const [redirect, setRedirect] = useState()
+  const [watchColor, setWatchColor] = useState(false)
+  const [isAdded, setIsAdded] = useState(false)
+  const [isSeen, setIsSeen] = useState(false)
+  const [seenColor, setSeenColor] = useState(false)
 
   const handleClick = () => {
     setRedirect(props.movie.id)
+  }
+
+  const handleSeen = () => {
+    const icon = isSeen
+    const color = seenColor
+    setIsSeen(!icon)
+    setSeenColor(!color)
+  }
+
+  const handleWatch = () => {
+    const icon = isAdded
+    const color = watchColor
+    setWatchColor(!color)
+    setIsAdded(!icon)
   }
 
   const renderRedirect = () => {
@@ -57,7 +75,13 @@ const MovieListItem = (props) => {
                   </Typography>
                 </Grid>
                 <Grid item>
-                  <Button size="medium" color="secondary" onClick={handleClick}>SEE MORE</Button>
+                  <Button size="small" className={classes.button} onClick={handleClick}>SEE MORE</Button>
+                  <Button size="small" className={classes.seenButton} style={{ backgroundColor: seenColor ? '#a9da71' : '#FFDF59' }}onClick={handleSeen}>
+                    <i className={classes.icon}>{ isSeen ? 'visibility' : 'visibility_off'}</i>&nbsp;SEEN
+                  </Button>
+                  <Button size="small" className={classes.watchButton} style={{ backgroundColor: watchColor ? '#a9da71' : '#FFDF59' }} onClick={handleWatch}>
+                    <i className={classes.icon}>{ isAdded ? 'check_box' : 'add_to_queue'}</i>&nbsp;WATCHLIST
+                  </Button>
                 </Grid>
               </Grid>
               {props.movie.movieTests.map(x => {
