e04e1b1996ee432968fd705fdda40be4bd4894e2
Jared <jared@needapart.co.nz>
2019-07-22 21:37:56 +1200

    Fix links on NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import { AppBar, Toolbar, Typography, Grid, Button, Badge } from '@material-ui/core'
+import { AppBar, Toolbar, Typography, Grid, Button, Badge, Box } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
 import SearchIcon from '@material-ui/icons/Search'
 
@@ -16,36 +16,34 @@ const StyledBadge = withStyles(() => ({
 function NavBar (props) {
   const classes = NavBarStyles(props)
   return (
-    <Link to="/">
-      <div className={classes.root}>
-        <AppBar position="fixed">
-          <Toolbar className={classes.toolbar}>
-            <Typography className={classes.logo}>
-              WB
-            </Typography>
+    <div className={classes.root}>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          <Link container to="/" className={`${classes.link} ${classes.logo}`}>WB</Link>
+          <Link container to="/" className={`${classes.link} ${classes.tagline}`}>
             <Typography className={classes.tagline}>
               CURATION THROUGH A LENS OF DIVERSITY
             </Typography>
-            <div className={classes.divide}>
-              <StyledBadge color="primary" badgeContent={props.watchlist.length} className={classes.margin}>
-                <Button className={classes.watchButton}><Link to="/movies/watchlist" className={classes.link}>Watchlist</Link></Button>
-              </StyledBadge>
-            </div>
-            <Button className={classes.navButton}><Link to="/quiz" className={classes.link}>Test A Movie</Link></Button>
-            <div className={classes.search}>
-              <Grid container spacing={1} alignItems="flex-end">
-                <Grid item>
-                  <SearchIcon className={classes.searchIcon}/>
-                </Grid>
-                <Grid item>
-                  <Search className={classes.search}/>
-                </Grid>
+          </Link>
+          <div className={classes.divide}>
+            <StyledBadge color="primary" badgeContent={props.watchlist.length} className={classes.margin}>
+              <Button className={classes.watchButton}><Link to="/movies/watchlist" className={classes.link}>Watchlist</Link></Button>
+            </StyledBadge>
+          </div>
+          <Button className={classes.navButton}><Link to="/quiz" className={classes.link}>Test A Movie</Link></Button>
+          <div className={classes.search}>
+            <Grid container spacing={1} alignItems="flex-end">
+              <Grid item>
+                <SearchIcon className={classes.searchIcon}/>
               </Grid>
-            </div>
-          </Toolbar>
-        </AppBar>
-      </div>
-    </Link>
+              <Grid item>
+                <Search className={classes.search}/>
+              </Grid>
+            </Grid>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
   )
 }
 
