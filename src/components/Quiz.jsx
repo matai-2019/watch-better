43cab33dfd5db61a71afd5c6cfa6555f308c7b63
Joanna Klimas <klimas.j.m@gmail.com>
2019-07-22 14:19:08 +1200

    change to functional component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,64 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+// import PropTypes from 'prop-types'
 import { Container, TextField, Button, Typography, List, ListItem } from '@material-ui/core'
 
-import { QuizStyles } from '../style/muiStyles'
 import QuizQuestion from './QuizQuestion'
 import data from '../data/quizQuestions.js'
 
-class Quiz extends React.Component {
-  state = {
-    title: '',
-    isVisible: true,
-    displayAnswers: false
+const Quiz = () => {
+
+  const [title, setTitle] = useState('')
+  const [isVisible, setVisible] = useState(true)
+  const [displayAnswer, setDisplayAnswer] = useState(false)
+
+  const test1 = []
+  const test2 = []
+  const test3 = []
+  const test4 = []
+  const test5 = []
+
+  const handleTitle = e => {
+    setTitle(e.target.value)
+    // this.setState({
+    //   title: e.target.value
+    // })
   }
 
-  test1 = []
-  test2 = []
-  test3 = []
-  test4 = []
-  test5 = []
-
-  // classes = (props) => {
-  //   QuizStyles(props)
-  // }
-
-  handleTitle = e => {
-    this.setState({
-      title: e.target.value
-    })
+  const onSubmit = () => {
+    setVisible(false)
+    // this.setState({
+    //   isVisible:false
+    // })
   }
 
-  onSubmit = () => {
-    this.setState({
-      isVisible:false
-    })
-  }
-
-  handleResult = () => {
-    this.setState({
-      displayAnswers: true
-    })
+  const handleResult = () => {
+    setDisplayAnswer(true)
+    // this.setState({
+    //   displayAnswers: true
+    // })
   }
   
-  handleChange = (value, id) => {
+  const handleChange = (value, id) => {
     switch (id) {
       case '1':
       case '2':
-        this.test1.push(value)
-        return this.test1
+        test1.push(value)
+        return test1
       case '3':
-        this.test2.push(value)
-        return this.test2 
+        test2.push(value)
+        return test2 
       case '4':
       case '5': 
       case '6':
-        this.test3.push(value)
-        return this.test3 
+        test3.push(value)
+        return test3 
       case '7':
       case '8': 
       case '9':
-        this.test4.push(value)
-        return this.test4
+        test4.push(value)
+        return test4
       case '10':
       case '11': 
       case '12':
@@ -67,82 +65,80 @@ class Quiz extends React.Component {
       case '15':
       case '16': 
       case '17':
-        this.test5.push(value)
-        return this.test5
+        test5.push(value)
+        return test5
       default:
-      console.log('whoops')
+        console.log('whoops')
     }
   }
 
-  result = (array) => {
+  const result = (array) => {
     return array.filter(value => value > 0).length
   }
 
-  bechdel = () => {
-    if (this.result(this.test1) === 2) {
+  const bechdel = () => {
+    if (result(this.test1) === 2) {
       return 'Passed the Bechdel Test' 
     } else return 'Failed the Bechdel Test'
   } 
     
-  reesDavies = () => {
-    if (this.result(this.test2) === 1) {
+  const reesDavies = () => {
+    if (result(this.test2) === 1) {
       return 'Passed the Rees-Davies Test'
     } else return 'Failed the Rees-Davies Test'
   } 
 
-  ko = () => {
-    if (this.result(this.test3) === 3) {
+  const ko = () => {
+    if (result(this.test3) === 3) {
       return 'Passed the Ko Test' 
     } else return 'Failed the Ko Test'
   } 
 
-  landau = () => {
-    if (this.result(this.test4) === 3) {
+  const landau = () => {
+    if (result(this.test4) === 3) {
       return 'Passed the Landau Test'
     } else return 'Failed the Landau Test'
   } 
 
-  feldman = () => {
-    if (this.result(this.test5) > 4) { 
+  const feldman = () => {
+    if (result(this.test5) > 4) { 
       return 'Passed the Feldman Test' 
     } else return 'Failed the Feldman Test'
   } 
 
-  render () {
-    if (this.state.displayAnswers) {
-      return (
-        <Container>
-          <Typography variant="h4" component="h4">
-            {`${this.state.title} has:`}
-          </Typography>
-          <List >
-            <ListItem><Typography variant="h5" gutterBottom>{this.bechdel()}</Typography></ListItem>
-            <ListItem><Typography variant="h5" gutterBottom>{this.reesDavies()}</Typography></ListItem>
-            <ListItem><Typography variant="h5" gutterBottom>{this.ko()}</Typography></ListItem>
-            <ListItem><Typography variant="h5" gutterBottom>{this.landau()}</Typography></ListItem>
-            <ListItem><Typography variant="h5" gutterBottom>{this.feldman()}</Typography></ListItem>
-          </List>
-        </Container>
-    )} else if (this.state.isVisible) {
-      return(
-        <Container>
-          <TextField
-            id="standard-search"
-            label="Movie Title"
-            type="search"
-            margin="normal"
-            onChange={this.handleTitle}
-          />
-          <Button onClick={this.onSubmit} variant="outlined" color="primary">Start</Button>
-          <br />
-          <br />
-        </Container>
-      )
-    } else if (!this.state.isVisible) {
-      return (
-        data.map(el => <QuizQuestion handleResult={this.handleResult} key={el.id} handleChange={this.handleChange} question={el.question} id={el.id} test={el.test} title={this.state.title}/>
+  if (displayAnswer) {
+    return (
+      <Container>
+        <Typography variant="h4" component="h4">
+          {`${title} has:`}
+        </Typography>
+        <List >
+          <ListItem><Typography variant="h5" gutterBottom>{bechdel()}</Typography></ListItem>
+          <ListItem><Typography variant="h5" gutterBottom>{reesDavies()}</Typography></ListItem>
+          <ListItem><Typography variant="h5" gutterBottom>{ko()}</Typography></ListItem>
+          <ListItem><Typography variant="h5" gutterBottom>{landau()}</Typography></ListItem>
+          <ListItem><Typography variant="h5" gutterBottom>{feldman()}</Typography></ListItem>
+        </List>
+      </Container>
+    )} else if (isVisible) {
+    return(
+      <Container>
+        <TextField
+          id="standard-search"
+          label="Movie Title"
+          type="search"
+          margin="normal"
+          onChange={handleTitle}
+        />
+        <Button onClick={onSubmit} variant="outlined" color="primary">Start</Button>
+        <br />
+        <br />
+      </Container>
+    )
+  } else if (!isVisible) {
+    return (
+      data.map(el => <QuizQuestion handleResult={handleResult} key={el.id} handleChange={handleChange} question={el.question} id={el.id} test={el.test} title={title}/>
       ))
-    }
   }
 }
 
