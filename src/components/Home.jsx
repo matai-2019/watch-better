85f3122b8ca669d7fa588d5781d3df82e388aea3
Raaya Pathare <raaya.pathare7@gmail.com>
2019-07-22 16:04:50 +1200

    sadfdasf

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 
 import RecommendationsNew from './RecommendationsNew'
 import MovieTestDetails from './MovieTestDetails'
-import Recommendations from './Recommendations'
+// import Recommendations from './Recommendations'
 import MovieDetails from './MovieDetails'
 import MovieList from './MovieList'
 import Watchlist from './Watchlist'
@@ -15,12 +15,10 @@ export default function Home () {
       {/* <Route path="/" exact component={Recommendations} /> */}
       <Route path="/" exact component={RecommendationsNew} />
       <Switch>
-        <Route path="/" exact component={MovieTestDetails} />
         <Route path="/movie/:id" component={MovieDetails} />
         <Route path="/movies/list" component={MovieList} />
         <Route path="/movies/watchlist" component={Watchlist} />
       </Switch>
-      <Route path="/" exact component={Recommendations} />
     </>
   )
 }
