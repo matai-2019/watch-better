9889c76f9396772f963ed271437a9d4bf0295ce9
Jared <jared@needapart.co.nz>
2019-07-23 11:58:49 +1200

    Setup comments router

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const pino = require('express-pino-logger')()
-const router = require('./routes/movies')
+const moviesRouter = require('./routes/movies')
+const commentsRouter = require('./routes/comments')
 const authRoutes = require('./routes/auth')
 
 const server = express()
@@ -9,7 +10,9 @@ server.use(express.json())
 server.use(pino)
 
 server.use(express.static('public'))
-server.use('/movie-api/', router)
+server.use('/movie-api/', moviesRouter)
+server.use('/comments-api/', commentsRouter)
+
 server.use('/', authRoutes)
 
 module.exports = server
