4a6d2c66c3a75438aff7b8a6ba8753db4a0cfebc
Joanna Klimas <klimas.j.m@gmail.com>
2019-07-23 17:02:10 +1200

    disable form when logged out

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import { Rating } from '@material-ui/lab'
 import { Paper, TextField, Box, Typography, Button, withStyles } from '@material-ui/core'
 
 import { CommentFormStyles } from '../style/muiStyles'
 import { addComment } from '../utilities/api'
+import { isAuthenticated } from '../auth'
 
 const StyledRating = withStyles({
   iconFilled: {
@@ -24,7 +26,6 @@ const CommentForm = ({ movieId }) => {
       comment,
       movieId
     }
-
     return addComment(userInput)
       .then(() => {
         setComment('')
@@ -36,13 +37,26 @@ const CommentForm = ({ movieId }) => {
       })
   }
 
+  const auth = () => {
+    if (isAuthenticated()) {
+      const filter = classes.paper
+      return filter
+    } else {
+      const filter = classes.filter
+      return filter
+    }
+  }
+
   return (
-    <Paper className={classes.paper}>
+    <Paper className={auth()}>
+      {isAuthenticated() ? null : <Typography variant="h5" component="p" className={classes.text}>Log in to leave a comment</Typography>}
+      <br/>
       <Box component="fieldset" borderColor="transparent">
         <Typography className={classes.text} component="legend">Rate the movie:</Typography>
         <StyledRating
           name="user rating"
           value={rating}
+          disabled={!isAuthenticated()}
           onChange={(e, newValue) => {
             setRating(newValue)
           }}
@@ -60,14 +74,21 @@ const CommentForm = ({ movieId }) => {
         className={classes.comment}
         margin="normal"
         variant="outlined"
+        disabled={!isAuthenticated()}
       />
       <br/>
-      <Button onClick={handleSubmit} variant="contained" color="primary">Submit</Button>
+      <Button onClick={handleSubmit} variant="contained" color="primary" disabled={!isAuthenticated()}>Submit</Button>
     </Paper>
   )
 }
 
-export default CommentForm
+const mapStateToProps = state => {
+  return {
+    movieId: state.movieDetails.id
+  }
+}
+
+export default connect(mapStateToProps)(CommentForm)
 
 CommentForm.propTypes = {
   movieId: PropTypes.number
