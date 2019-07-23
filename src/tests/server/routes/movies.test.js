e0b5337587546ef4532e1005c31be8c351948210
Jared <jared@needapart.co.nz>
2019-07-23 14:21:23 +1200

    Add test for route test for get comments

--- src/tests/server/routes/movies.test.js
+++ src/tests/server/routes/movies.test.js
@@ -3,38 +3,49 @@ const server = require('../../../../server/server')
 
 jest.mock('../../../../server/db/db')
 
-test('tests work', () => {
-  expect(true).toBeTruthy()
-})
+describe('Route tests', () => {
+  it('tests work', () => {
+    expect(true).toBeTruthy()
+  })
 
-test("GET('/') responds with status 200", () => {
-  return request(server)
-    .get('/movie-api/')
-    .then(res => expect(res.status).toBe(200))
-    .catch(err => expect(err).toBeNull())
-})
+  it("GET('/') responds with status 200", () => {
+    return request(server)
+      .get('/movie-api/')
+      .then(res => expect(res.status).toBe(200))
+      .catch(err => expect(err).toBeNull())
+  })
 
-test("GET('/') returns an array of 10 movies", () => {
-  return request(server)
-    .get('/movie-api/')
-    .then(res => {
-      expect(res.body).toHaveLength(2)
-    })
-    .catch(err => expect(err).toBeNull())
-})
+  it("GET('/') returns an array of 10 movies", () => {
+    return request(server)
+      .get('/movie-api/')
+      .then(res => {
+        expect(res.body).toHaveLength(2)
+      })
+      .catch(err => expect(err).toBeNull())
+  })
 
-test("GET('/':id) responds with status 200", () => {
-  return request(server)
-    .get('/movie-api/1')
-    .then(res => expect(res.status).toBe(200))
-    .catch(err => expect(err).toBeNull())
-})
+  it("GET('/':id) responds with status 200", () => {
+    return request(server)
+      .get('/movie-api/1')
+      .then(res => expect(res.status).toBe(200))
+      .catch(err => expect(err).toBeNull())
+  })
 
-test("GET('/search/:query':id)responds with status 200", () => {
-  return request(server)
-    .get('/movie-api/search/fight+club')
-    .end((err, res) => {
-      expect(err).toBeNull()
-      expect(res.status).toBe(200)
-    })
+  it("GET('/search/:query':id)responds with status 200", () => {
+    return request(server)
+      .get('/movie-api/search/fight+club')
+      .end((err, res) => {
+        expect(err).toBeNull()
+        expect(res.status).toBe(200)
+      })
+  })
+
+  it("GET('/comments/:id') responds with status 200", () => {
+    return request(server)
+      .get('/movie-api/comments/2')
+      .end((err, res) => {
+        expect(err).toBeNull()
+        expect(res.status).toBe(200)
+      })
+  })
 })
