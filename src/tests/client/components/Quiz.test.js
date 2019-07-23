89e565443752fcae6da28aabff2f2b832ee595d5
Joanna Klimas <klimas.j.m@gmail.com>
2019-07-23 19:39:59 +1200

    fix Quiz tests

--- src/tests/client/components/Quiz.test.js
+++ src/tests/client/components/Quiz.test.js
@@ -1,42 +1,53 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { ThemeProvider } from '@material-ui/styles'
 import { Button, ListItem } from '@material-ui/core'
 import Enzyme, { mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
 
+import { theme } from '../../../style/muiStyles'
 import Quiz from '../../../components/Quiz'
+import MovieTestItem from '../../../components/MovieTestDetails'
 import QuizQuestion from '../../../components/QuizQuestion'
 
 Enzyme.configure({ adapter: new Adapter() })
 
+const testStore = { quizAnswer: { id: '1', answer: 'yes' } }
+const mockStore = configureStore([thunk])(testStore)
+
 describe('Quiz component tests', () => {
-  it('<Quiz /> displays a button on a initial render', () => {
-    const wrapper = mount(<Quiz />)
+  it('<Quiz /> displays two buttons on a initial render', () => {
+    const wrapper = mount(
+      <Provider store={mockStore}>
+        <ThemeProvider theme={theme}>
+          <Quiz/>
+        </ThemeProvider>
+      </Provider>)
     const actual = wrapper.find(Button)
-    expect(actual).toHaveLength(1)
+    expect(actual).toHaveLength(2)
   })
-  it('<Quiz> component changes this.state.isVisible to false on start button click', () => {
-    const expected = false
-    const wrapper = mount(<Quiz/>)
-    const startButton = wrapper.find('button').at(0)
-
-    startButton.simulate('click')
-
-    expect(wrapper.state().isVisible).toEqual(expected)
-  })
-  it('<Quiz> component displays 17 <QuizQuestion> components when state.isVisible is set to false', () => {
+  it('<Quiz> component displays 17 <QuizQuestion> components', () => {
     const expected = 17
-    const wrapper = mount(<Quiz/>)
-    wrapper.setState({ isVisible: false })
-
+    const wrapper = mount(
+      <Provider store={mockStore}>
+        <ThemeProvider theme={theme}>
+          <Quiz/>
+        </ThemeProvider>
+      </Provider>)
     const actual = wrapper.find(QuizQuestion)
     expect(actual).toHaveLength(expected)
   })
-  it('<Quiz> component displays 5 <ListItem> components when state.displayAnswers is set to true', () => {
-    const expected = 5
-    const wrapper = mount(<Quiz/>)
-    wrapper.setState({ displayAnswers: true })
-
-    const actual = wrapper.find(ListItem)
+  it('<Quiz> component displays <MovieTestDetails> component', () => {
+    const expected = 1
+    const wrapper = mount(
+      <Provider store={mockStore}>
+        <ThemeProvider theme={theme}>
+          <Quiz/>
+        </ThemeProvider>
+      </Provider>)
+    const actual = wrapper.find(MovieTestItem)
     expect(actual).toHaveLength(expected)
   })
 })
