9571adbfcce568ae7a9dceade9911b9cc133d806
Anisha Kalidas <anishadhana@gmail.com>
2019-07-24 17:16:13 +1200

    FIXED BUG

--- src/reducers/watchlist.js
+++ src/reducers/watchlist.js
@@ -1,12 +1,9 @@
 import { GET_WATCHLIST_SUCCESS } from '../actions/watchlist'
-import { GET_SEENLIST_SUCCESS } from '../actions/seenList'
 
 const watchlist = (state = [], action) => {
   switch (action.type) {
     case GET_WATCHLIST_SUCCESS:
       return action.watchList
-    case GET_SEENLIST_SUCCESS:
-      return action.seenlist
     default:
       return state
   }
