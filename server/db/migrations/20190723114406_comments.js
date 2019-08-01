fadd5654fd8fc327ad0f262a7c69b7a16fcbdfab
Jared <jared@needapart.co.nz>
2019-08-01 18:42:20 +1200

    Remove duplicat refernces

--- server/db/migrations/20190723114406_comments.js
+++ server/db/migrations/20190723114406_comments.js
@@ -1,8 +1,8 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('comments', table => {
     table.increments('id').primary()
-    table.integer('movie_id').references('movies.id')
-    table.integer('user_id').references('users.id')
+    table.integer('movie_id')
+    table.integer('user_id')
     table.string('comment')
     table.integer('user_rating')
     table.datetime('created').defaultTo(knex.fn.now())
