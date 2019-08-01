726fbe2eab8f53829343a4bd1a18d30c8d0bce63
Jared <jared@needapart.co.nz>
2019-08-01 22:12:08 +1200

    float becomes real

--- server/db/migrations/20190718122120_movies.js
+++ server/db/migrations/20190718122120_movies.js
@@ -5,7 +5,7 @@ exports.up = knex =>
     table.integer('API_movie_id')
     table.boolean('recommended')
     table.string('image')
-    table.float('rating')
+    table.real('rating')
   })
 
 exports.down = knex => knex.schema.dropTable('movies')
