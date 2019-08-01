0289cc7b87f100ee8d2e77d940f512b95609ef84
Jared <jared@needapart.co.nz>
2019-08-01 18:30:05 +1200

    Revert "DB use foreign keys to reference relationships"
    This reverts commit cb3b8e87071bcea9fa6372be00fada1fbc5b5d91.

--- server/db/migrations/20190723134610_watchlist.js
+++ server/db/migrations/20190723134610_watchlist.js
@@ -1,8 +1,8 @@
 exports.up = knex =>
   knex.schema.createTable('watchlist', table => {
     table.increments('id').primary()
-    table.foreign('movie_id').references('movies.id')
-    table.foreign('user_id').references('users.id')
+    table.integer('movie_id').references('movies.id')
+    table.integer('user_id').references('users.id')
   })
 
 exports.down = knex => knex.schema.dropTable('watchlist')
