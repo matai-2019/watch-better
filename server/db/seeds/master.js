0f593a6e70b3afb599c502b4e0ce8c84fed562fb
Jared <jared@needapart.co.nz>
2019-08-04 07:23:21 +1200

    Compile seeds in to one file

---
+++ server/db/seeds/master.js
@@ -0,0 +1,18 @@
+const moviesData = require('../../data/movies')
+const movieTestsData = require('../../data/movie_tests')
+const usersData = require('../../data/users')
+const commentsData = require('../../data/comments')
+
+exports.seed = knex => {
+  // Deletes ALL existing entries
+  return knex('movies').del()
+    .then(() => knex('movie_tests').del())
+    .then(() => knex('users').del())
+    .then(() => knex('comments').del())
+    .then(() => knex('watchlist').del())
+    .then(() => knex('seenlist').del())
+    .then(() => knex('movie').insert(moviesData))
+    .then(() => knex('movie_tests').insert(movieTestsData))
+    .then(() => knex('users').insert(usersData))
+    .then(() => knex('comments').insert(commentsData))
+}
