8a7261c3d671d707e5653343158e940bda2a6029
Jared <jared@needapart.co.nz>
2019-08-04 07:13:07 +1200

    Make comments seed data

---
+++ server/data/comments.js
@@ -0,0 +1,5 @@
+module.exports = [
+  { id: 1, movie_id: 2, user_id: 1, comment: 'This movie is awesome!', user_rating: 4 },
+  { id: 2, movie_id: 2, user_id: 2, comment: 'This movie is alright.', user_rating: 3 },
+  { id: 3, movie_id: 2, user_id: 3, comment: 'This movie sucks!', user_rating: 2 }
+]
