915fed7628512236676572bbd185b03121e73b57
Jared <jared@needapart.co.nz>
2019-08-03 16:35:52 +1200

    Add logic for express.static() for prod vs dev

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const pino = require('express-pino-logger')()
 const router = require('./routes/movies')
 const authRoutes = require('./routes/auth')
@@ -10,7 +11,16 @@ const server = express()
 server.use(express.json())
 server.use(pino)
 
-server.use(express.static('public'))
+if (process.env.NODE_ENV === 'development') {
+  server.use(express.static('public'))
+} else if (process.env.NODE_ENV === 'production') {
+  server.use(express.static(path.join(__dirname, '../build')))
+
+  server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'))
+  })
+}
+
 server.use('/movie-api/watchlist', watchlist)
 server.use('/movie-api/seenlist', seenlist)
 server.use('/movie-api/', router)
