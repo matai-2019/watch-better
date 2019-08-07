8368e8a6086cc56c031ecbcda864a0fabda31c82
David Brown <david.brown@windowslive.com>
2019-08-07 15:30:22 +1200

    sdffds

--- server/db/seeds/02_users.js
+++ server/db/seeds/02_users.js
@@ -1,12 +1,12 @@
-exports.seed = (knex, Promise) => {
-  // Deletes ALL existing entries
-  return knex('users').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('users').insert([
-        { id: 1, firstname: 'Raaya', lastname: 'Pathare', email: 'rp@wb.co.nz', hash: '$argon2id$v=19$m=65536,t=2,p=1$S/6JKvkMLzhE1Hh5eSR6iQ$2/8yN7iRUOoQwC/OyVaJl+pLPtHZhsImtEl8fomLGcA' },
-        { id: 2, firstname: 'Jared', lastname: 'Tasker', email: 'jt@wb.co.nz', hash: '$argon2id$v=19$m=65536,t=2,p=1$bT0hN0sYiryIk1chdX+mrw$/rW20cfYqjp+np6sUtpDSyVgXc+DN0XFq4YNBKpCKY0' },
-        { id: 3, firstname: 'Joanna', lastname: 'Klimas', email: 'jk@wb.co.nz', hash: '$argon2id$v=19$m=65536,t=2,p=1$bc5ToEJz11Ce4E7/QceiRQ$sAUR4wroF2vo1w413PKv4lhD+AfAa+3bt2FML2W7gII' }
-      ])
-    })
-}
+// exports.seed = (knex, Promise) => {
+//   // Deletes ALL existing entries
+//   return knex('users').del()
+//     .then(function () {
+//       // Inserts seed entries
+//       return knex('users').insert([
+//         { id: 1, firstname: 'Raaya', lastname: 'Pathare', email: 'rp@wb.co.nz', hash: '$argon2id$v=19$m=65536,t=2,p=1$S/6JKvkMLzhE1Hh5eSR6iQ$2/8yN7iRUOoQwC/OyVaJl+pLPtHZhsImtEl8fomLGcA' },
+//         { id: 2, firstname: 'Jared', lastname: 'Tasker', email: 'jt@wb.co.nz', hash: '$argon2id$v=19$m=65536,t=2,p=1$bT0hN0sYiryIk1chdX+mrw$/rW20cfYqjp+np6sUtpDSyVgXc+DN0XFq4YNBKpCKY0' },
+//         { id: 3, firstname: 'Joanna', lastname: 'Klimas', email: 'jk@wb.co.nz', hash: '$argon2id$v=19$m=65536,t=2,p=1$bc5ToEJz11Ce4E7/QceiRQ$sAUR4wroF2vo1w413PKv4lhD+AfAa+3bt2FML2W7gII' }
+//       ])
+//     })
+// }
