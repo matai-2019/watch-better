31dc5136797c5de5693ba35df4080dd0f9f99812
David Brown <david.brown@windowslive.com>
2019-08-07 15:21:28 +1200

    dfsdf

--- server/db/migrations/02_20190722185524_users.js
+++ server/db/migrations/02_20190722185524_users.js
@@ -1,6 +1,6 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('users', table => {
-    table.increments('id')
+    table.increments('id').primary()
     table.string('firstname')
     table.string('lastname')
     table.string('email')
