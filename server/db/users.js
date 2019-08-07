0ca1fbab3144a651d21c7924ab51013713df64b7
David Brown <david.brown@windowslive.com>
2019-08-07 15:11:43 +1200

    sdf

--- server/db/users.js
+++ server/db/users.js
@@ -1,7 +1,7 @@
 const connection = require('./connection')
 const { generateHash } = require('../auth')
 
-function createUser ({ firstName, lastName, email, password }, db = connection) {
+function createUser ({ firstname, lastname, email, password }, db = connection) {
 
   return userExists(email, db)
     .then(exists => {
